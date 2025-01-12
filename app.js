14b447a77bfc399871b367e6422d5e96844729b4
piyushjoshi <piyushj8989@gmail.com>
2025-01-12 00:02:26 -0800

    fixed bug

--- app.js
+++ app.js
@@ -81,7 +81,7 @@ app.use('/', viewRouter);
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 app.use('/api/v1/reviews', reviewRouter);
-// app.use('/api/v1/bookings', bookingRouter);
+app.use('/api/v1/bookings', bookingRouter);
 
 
 // Catch-all route
