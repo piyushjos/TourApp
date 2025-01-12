3260071f61f047792ee776e25b042940ebf0e926
piyushjoshi <piyushj8989@gmail.com>
2025-01-11 21:02:18 -0800

    fix error

--- controllers/viewsController.js
+++ controllers/viewsController.js
@@ -6,8 +6,13 @@ const AppError = require('../utils/appError');
 
 exports.getOverview = catchAsync(async (req, res, next) => {
   // 1) Get tour data from collection
+  console.log(new Date().toLocaleString(), ' database call for tours===>');
   const tours = await Tour.find();
-
+  console.log(
+    new Date().toLocaleString(),
+    ' database call success for tours===>',
+    tours
+  );
   // 2) Build template
   // 3) Render that template using tour data from 1)
   res.status(200).render('overview', {
